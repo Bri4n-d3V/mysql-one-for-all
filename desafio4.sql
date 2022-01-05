525d950b69b0fbe2497d6ebcfdc75602f76dac4a
BrianCaetano <brian-caetano@hotmail.com>
2022-01-05 15:56:44 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT u.usuario,
+IF(MAX(YEAR(h.data_reproducao)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h 
+ON u.usuario_id = h.usuario_id
+GROUP BY u.usuario
+ORDER BY u.usuario;
